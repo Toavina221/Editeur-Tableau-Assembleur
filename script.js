340bf8e48aac32578be75988fa18aead4b778d4c
Toavina Rova <toavinarovamampionina@gmail.com>
2025-12-19 09:26:43 +0300

    Refactor tab functionality and LaTeX formatting

--- script.js
+++ script.js
@@ -264,7 +264,9 @@ document.querySelector(".cut-btn").addEventListener("click", async () => {
   await navigator.clipboard.writeText(text); // copie
   assemblerInput.value ="";                      // supprime (effet couper)
    assemblerCode.innerText = "";
+     arrayCode.innerHTML="";
 });
+
 
 
 
